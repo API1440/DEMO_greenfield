bc06276ca8170ca597dfceed0fef91dc89c6cc63
Vishal Singh <vishal.singh@apisero.com>
2021-08-13 10:47:07 +0530

    saved six lines

--- Demo.java
+++ Demo.java
@@ -4,5 +4,11 @@ public static void main(String[] args){
   System.out.println("hello world second time");
   System.out.println("hello world third time");
   System.out.println("Added to copy 1");
+  System.out.println("-----------------");
+  System.out.println("api line 1");
+  System.out.println("api line 2");
+  System.out.println("api line 3");
+  System.out.println("api line 4");
+  System.out.println("api line 5");
   }
 }
